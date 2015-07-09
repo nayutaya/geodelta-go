cd1074bb8abab89535352bc4a358b2e292179352
Yuya Kato <yuyakato@gmail.com>
2015-07-09 15:18:30 +0900

    メルカトル座標を緯度経度に変換する処理を追加

--- geodelta/projector.go
+++ geodelta/projector.go
@@ -3,6 +3,7 @@ package geodelta
 import "math"
 
 const DEG2RAD float64 = math.Pi / 180.0 // 度をラジアンに変換するための係数
+const RAD2DEG float64 = 180.0 / math.Pi // ラジアンを度に変換するための係数
 
 // 緯度をメルカトルY座標に変換する
 //   -90.0 <= lat <= +90.0
@@ -18,28 +19,30 @@ func LngToMx(lng float64) float64 {
 	return lng / 180.0
 }
 
+// メルカトルY座標を緯度に変換する
+//    -1.0 <= my  <=  +1.0
+//   -90.0 <= lat <= +90.0
+func MyToLat(my float64) float64 {
+	return math.Asin(math.Tanh(my*math.Pi)) * RAD2DEG
+}
+
+// メルカトルX座標を経度に変換する
+//     -1.0 <= mx  <=   +1.0
+//   -180.0 <= lng <= +180.0
+func MxToLng(mx float64) float64 {
+	if mx < -1.0 {
+		mx += 2.0
+	}
+	if mx > +1.0 {
+		mx -= 2.0
+	}
+	return mx * 180.0
+}
+
 /* Ruby
 module GeoDelta
   module Projector
-    RAD2DEG      = 180.0 / Math::PI # ラジアンを度に変換するための係数
     DELTA_HEIGHT = Math.sqrt(0.75)  # 一辺を1.0とする正三角形の高さ
-
-
-    # メルカトルY座標を緯度に変換する
-    #    -1.0 <= my  <=  +1.0
-    #   -90.0 <= lat <= +90.0
-    def self.my_to_lat(my)
-      return Math.asin(Math.tanh(my * Math::PI)) * RAD2DEG
-    end
-
-    # メルカトルX座標を経度に変換する
-    #     -1.0 <= mx  <=   +1.0
-    #   -180.0 <= lng <= +180.0
-    def self.mx_to_lng(mx)
-      mx  = (mx % 2.0) - 2.0
-      mx += 2.0 if mx < -1.0
-      return mx * 180.0
-    end
 
     # メルカトルY座標から正規化Y座標に変換する
     #    -1.0 <= my <=  +1.0
