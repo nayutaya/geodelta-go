1ef734c03f0239db55d9bf96a0ecbcad4a1a879c
Yuya Kato <yuyakato@gmail.com>
2015-07-10 16:16:30 +0900

    コメントを整理

--- geodelta/projector.go
+++ geodelta/projector.go
@@ -2,27 +2,23 @@ package geodelta
 
 import "math"
 
-type Lat float64
-type Lng float64
-type My float64
-type Mx float64
-type Ny float64
-type Nx float64
+type Lat float64 //  -90.0 <= lat <=  +90.0
+type Lng float64 // -180.0 <= lng <= +180.0
+type My float64  //   -1.0 <= my  <=   +1.0
+type Mx float64  //   -1.0 <= mx  <=   +1.0
+type Ny float64  //  -12.0 <= ny  <=  +12.0
+type Nx float64  //  -12.0 <= nx  <=  +12.0
 
 const DEG2RAD = math.Pi / 180.0         // 度をラジアンに変換するための係数
 const RAD2DEG = 180.0 / math.Pi         // ラジアンを度に変換するための係数
 const DELTA_HEIGHT = 0.8660254037844386 // 一辺を1.0とする正三角形の高さ math.Sqrt((1*1)-(0.5*0.5))
 
 // 緯度をメルカトルY座標に変換する
-//   -90.0 <= lat <= +90.0
-//    -1.0 <= my  <=  +1.0
 func (lat Lat) ToMy() My {
 	return My(math.Atanh(math.Sin(float64(lat)*DEG2RAD)) / math.Pi)
 }
 
 // 経度をメルカトルX座標に変換する
-//   -180.0 <= lng <= +180.0
-//     -1.0 <= mx  <=   +1.0
 func (lng Lng) ToMx() Mx {
 	return Mx(float64(lng) / 180.0)
 }
@@ -39,43 +35,31 @@ func NormalizeM(m float64) float64 {
 }
 
 // メルカトルY座標を緯度に変換する
-//    -1.0 <= my  <=  +1.0
-//   -90.0 <= lat <= +90.0
 func (my My) ToLat() Lat {
 	return Lat(math.Asin(math.Tanh(NormalizeM(float64(my))*math.Pi)) * RAD2DEG)
 }
 
 // メルカトルX座標を経度に変換する
-//     -1.0 <= mx  <=   +1.0
-//   -180.0 <= lng <= +180.0
 func (mx Mx) ToLng() Lng {
 	return Lng(NormalizeM(float64(mx)) * 180.0)
 }
 
 // メルカトルY座標から正規化Y座標に変換する
-//    -1.0 <= my <=  +1.0
-//   -12.0 <= ny <= +12.0
 func (my My) ToNy() Ny {
 	return Ny(float64(my) / DELTA_HEIGHT * 12.0)
 }
 
 // メルカトルX座標から正規化X座標に変換する
-//    -1.0 <= my <=  +1.0
-//   -12.0 <= ny <= +12.0
 func (mx Mx) ToNx() Nx {
 	return Nx(float64(mx) * 12.0)
 }
 
 // 正規化Y座標からメルカトルY座標に変換する
-//   -12.0 <= ny <= +12.0
-//    -1.0 <= my <=  +1.0
 func (ny Ny) ToMy() My {
 	return My(float64(ny) / 12.0 * DELTA_HEIGHT)
 }
 
 // 正規化X座標からメルカトルX座標に変換する
-//   -12.0 <= nx <= +12.0
-//    -1.0 <= mx <=  +1.0
 func (nx Nx) ToMx() Mx {
 	return Mx(float64(nx) / 12.0)
 }
