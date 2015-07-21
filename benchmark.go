b80a3db671f623b909d3e2c96e279b6eee758caf
Yuya Kato <yuyakato@gmail.com>
2015-07-21 12:44:20 +0900

    ベンチマーク処理を変更

--- benchmark.go
+++ benchmark.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/nayutaya/geodelta-go/geodelta/encoder"
+	"github.com/nayutaya/geodelta-go/geodelta"
 	"math/rand"
 	"time"
 )
 
+/*
 func main() {
 	world := encoder.DeltaIds{0, 1, 2, 3, 4, 5, 6, 7}
 	sub := encoder.DeltaIds{0, 1, 2, 3}
@@ -21,4 +22,34 @@ func main() {
 	}
 	end_time := time.Now().UnixNano()
 	fmt.Println("time=", (end_time-start_time)/1000/1000)
+}
+*/
+
+func main() {
+	rand.Seed(1)
+	start_time := time.Now().UnixNano()
+
+	ok, ng := 0, 0
+	for i := 0; i < 50000; i++ {
+		lat1 := rand.Float64()*(85*2) - 85
+		lng1 := rand.Float64()*(180*2) - 180
+		for level := byte(1); level <= 20; level++ {
+			code1 := geodelta.GetDeltaCode(lat1, lng1, level)
+			lat2, lng2 := geodelta.GetCenterFromDeltaCode(code1)
+			code2 := geodelta.GetDeltaCode(lat2, lng2, level)
+			// fmt.Printf("lat1=%f,lng1=%f\n", lat1, lng1)
+			// fmt.Printf("lat2=%f,lng2=%f\n", lat2, lng2)
+			// fmt.Printf("code1=%s\n", code1)
+			// fmt.Printf("code2=%s\n", code2)
+			if code1 == code2 {
+				ok++
+			} else {
+				ng++
+			}
+		}
+	}
+	end_time := time.Now().UnixNano()
+	fmt.Printf("time=%d\n", (end_time-start_time)/1000/1000)
+	fmt.Printf("ok=%d\n", ok)
+	fmt.Printf("ng=%d\n", ng)
 }
